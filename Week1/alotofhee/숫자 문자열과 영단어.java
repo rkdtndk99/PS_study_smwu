b124e7a72b67a29495ff571f94f345a9e55e0818
alotofhee <55499429+alotofhee@users.noreply.github.com>
2022-03-25 22:58:43 +0900

    03/25(금)_PRGM_Lv.1

--- Week1/alotofhee/숫자 문자열과 영단어.java
+++ Week1/alotofhee/숫자 문자열과 영단어.java
@@ -9,3 +9,29 @@ class Solution {
         return answer;
     }
 }
+
+/* 해결 전략 
+숫자에 대응되는 영단어 배열 생성 (인덱스 값과 대응되는 영단어 숫자가 같게 됨)
+s 문자열에서 numArray[i] 값과 대응되는 문자열을 replace 사용하여 i로 대체함.
+(이 때 i를 string으로 타입 일치시켜주기)
+문자열 s를 int로 변환
+*/
+
+//다른사람의 풀이
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        StringBuilder sb = new StringBuilder("");
+        int len = s.length();
+        String[] digits = {"0","1","2","3","4","5","6","7","8","9"};
+        String[] alphabets = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+
+        for(int i=0; i<10; i++){
+            s = s.replaceAll(alphabets[i],digits[i]);
+        }
+
+        return Integer.parseInt(s);
+    }
+}
