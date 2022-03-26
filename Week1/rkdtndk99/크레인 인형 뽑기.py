48d32dbd7921b1c3f28af00cbd79cb199a2e4064
Suah Kang <christinekang0123@naver.com>
2022-03-26 17:04:19 +0900

    03/25(금)_PRGM_Lv.1(수정)

--- Week1/rkdtndk99/크레인 인형 뽑기.py
+++ Week1/rkdtndk99/크레인 인형 뽑기.py
@@ -1,13 +1,39 @@
+# 내 풀이
 def solution(board, moves):
-    bucket = []
-    answer = []
-    for move in moves:
-        for i in range(len(board)):
-            if board[i][move-1] > 0:
-                bucket.append(board[i][move-1])
-                board[i][move-1] = 0
-                if bucket[-1:] == bucket[-2:-1]:
-                    answer += bucket[-1:]
-                    bucket = bucket[:-2]
+    answer = 0
+    stack = []
+
+    for i in range(len(moves)):
+        col = moves[i] - 1
+        for j in range(len(board)):
+            if board[j][col] != 0:
+                out = board[j][col]
+                board[j][col] = 0
+                stack.append(out)
+                if len(stack) >= 2:
+                    if stack[-1] == stack[-2]:
+                        stack.pop()
+                        stack.pop()
+                        answer += 2
                 break
-    return len(answer)*2
+    return answer
+
+# 다른 사람 풀이
+def solution(board, moves):
+    stacklist = []
+    answer = 0
+
+    for i in moves:
+        for j in range(len(board)):
+            if board[j][i-1] != 0:
+                stacklist.append(board[j][i-1])
+                board[j][i-1] = 0
+
+                if len(stacklist) > 1:
+                    if stacklist[-1] == stacklist[-2]:
+                        stacklist.pop(-1)
+                        stacklist.pop(-1)
+                        answer += 2
+                break
+
+    return answer
